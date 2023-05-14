3d3388691a2602f73e6bdabb1904c4261928adf7
OmarSuede <omarghina20@gmail.com>
2023-05-13 16:46:01 -0800

    added UI functions

--- game.py
+++ game.py
@@ -1,24 +1,49 @@
 import pygame
 import pygame_gui
+import random
 
-from constants import UIDefault
+from constants import Default
 import rule
-import ant
 import cell
 
 
 from rule import Rule
 from squaregrid import SquareGrid
+from ant import Ant
 
 
 class Game:
     def __init__(self):
         self.stepsize = Default.stepsize
-        self.rule = Rule()
-        self.rules = [rule((0, 0, 0), "R"), rule((255, 255, 255), "L")]
-        self.grid = SquareGrid()
+        self.rules = [Rule((0, 0, 0), "R"), Rule((255, 255, 255), "L")]
+        self.grid = SquareGrid(Default.GW, Default.GH)
+        self.ants = [Ant((Default.GW // 2, Default.GH // 2), 1)]
         pass
 
+    def gridSlider(self):
+        self.grid = SquareGrid(Default.GW, Default.GH)
+
+    def antSlider(self):
+        self.ants = []
+        for i in range(Default.ants):
+            self.ants.append(
+                Ant(
+                    (random.randint(0, Default.GW), random.randint(0, Default.GH)),
+                    random.randint(0, 4),
+                )
+            )
+
+    def randomAnts(self):
+        for i in range(Default.ants):
+            self.ants[i] = Ant(
+                (random.randint(0, Default.GW), random.randint(0, Default.GH)),
+                random.randint(0, 4),
+            )
+
+    def inputRule(self):
+        self.rules = []
+        for i in range(len(Default.rules)):
+            self.rules.append(Rule((0, 0, 0), Default.rules[i]))
 
     def step(self):
         pass
