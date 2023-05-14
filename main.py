7ae9667f15340c25083101ddaea59dfed7adc331
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-05-14 02:21:13 -0700

    optional delay for ui updates in case performance becomes low

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ import rule
 import ant
 import cell
 
-
 from constants import Constant
 
 pygame.init()
 pygame.display.set_caption(Constant.title)
 
 game = game.Game()
-renderer = renderer.Renderer()
+renderer = renderer.Renderer(game)
 ui = ui.UI(game, renderer)
 clock = pygame.time.Clock()
 is_running = True
 
+ui_last_time = pygame.time.get_ticks()
+
 while is_running:
     time_delta = clock.tick(Constant.FPS)/1000.0
+    ui_time_since_last_update = pygame.time.get_ticks() - ui_last_time
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
@@ -29,15 +31,13 @@ while is_running:
             if event.key == pygame.K_ESCAPE:
                 is_running = False
 
-        if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            ui.eventupdate(event)
-
+        ui.eventupdate(event)
         ui.manager.process_events(event)
 
     game.update(time_delta)
     ui.manager.update(time_delta)
     
-    renderer.render(game)
+    renderer.render()
     ui.draw()
     
     pygame.display.update()
