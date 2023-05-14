1f3b2866c2ebc842f54e65a8f7db33e69d8bdc5c
OmarSuede <omarghina20@gmail.com>
2023-05-13 16:45:26 -0800

    updated default vals

--- constants.py
+++ constants.py
@@ -1,25 +1,27 @@
 # hardcoded constants
 class Constant:
     # WINDOW
-    title = 'Langton\'s Ant'
+    title = "Langton's Ant"
     W = 1700
-    aspectratio = 9/16
-    H = int(W*aspectratio)
-    
+    aspectratio = 9 / 16
+    H = int(W * aspectratio)
+
     # UI
     uioffset = 0.8
     uisize = 0.2
-    
+
     # FRAMES
     FPS = 60
 
     # COLORS
-    backcolor = (0,0,0)
-    uibackcolor = (150,150,150)
+    backcolor = (0, 0, 0)
+    uibackcolor = (150, 150, 150)
+
 
 # default values / can be updated
 class Default:
-    stepsize = 1 #per frame
+    stepsize = 1  # per frame
     GW = 100
     GH = 100
-    
+    ants = 1
+    rules = "RL"
